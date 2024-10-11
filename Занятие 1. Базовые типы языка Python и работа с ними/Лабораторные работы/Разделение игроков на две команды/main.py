90c82d80329bce639b3afd14f3182df550eb5c80
Elena <whohasbudunec@gmail.com>
2024-10-11 18:21:49 +0300

    Lab1

--- Занятие 1. Базовые типы языка Python и работа с ними/Лабораторные работы/Разделение игроков на две команды/main.py
+++ Занятие 1. Базовые типы языка Python и работа с ними/Лабораторные работы/Разделение игроков на две команды/main.py
@@ -1,3 +1,8 @@
 list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
-
+number_of_players = len(list_players)
+mid = number_of_players // 2
+com1 = list_players[:mid]
+com2 = list_players[mid:]
+print(com1)
+print(com2)
 # TODO Разделите участников на две команды
--- Занятие 1. Базовые типы языка Python и работа с ними/Составные типы данных/Явное приведение типов в Python/main.py
+++ Занятие 1. Базовые типы языка Python и работа с ними/Составные типы данных/Явное приведение типов в Python/main.py
@@ -1,3 +1,5 @@
 str_ = "sdkfjsdkjfnksjdnfkjsdnfkjsndfkjsndfkjsndfkjsnd"
 
-# TODO найдите и распечатайте количество уникальных символов в строке
+unic_str = set(str_)
+
+print(len(unic_str))# TODO найдите и распечатайте количество уникальных символов в строке
